97604c3f72d6ac6c42acd37d1a0f1c6099aab72e
LittleWaterDrops <nic6@dangamsoft.com>
2022-03-14 10:41:23 +0900

    데이터베이스 3월 변경

--- server/src/db/DBTables.tsx
+++ server/src/db/DBTables.tsx
@@ -1,7 +1,7 @@
 const DBTables = {
   MEMBER_LIST: "DGmembers",
   USAGE_LIST: "card_usage_statement",
-  USE_DATA: "DGcard_use_data_2202_copy_test",
+  USE_DATA: "DGcard_use_data_2203",
 }
 
 export default DBTables
