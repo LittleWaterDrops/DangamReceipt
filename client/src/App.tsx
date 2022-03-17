8c551d1b867efb9eba2ab24adf2e52ef2cbf4168
LittleWaterDrops <nic6@dangamsoft.com>
2022-03-17 11:15:35 +0900

    스크린 배경 패딩 부여

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,21 @@ import Header from "./pages/main/components/Header"
 import AddDataScreen from "./pages/main/screens/AddDataScreen"
 import MainScreen from "./pages/main/screens/MainScreen"
 import ManagementScreen from "./pages/main/screens/ManagementScreen"
+import "./App.css"
 
 const App: React.FC = () => {
   return (
-    <div>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<MainScreen />} />
-        <Route path="/addData" element={<AddDataScreen />} />
-        <Route path="/manage" element={<ManagementScreen />} />
-        <Route path="/addData/:dataNumber" element={<AddDataScreen />} />
-      </Routes>
-    </div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<MainScreen />} />
+          <Route path="/addData" element={<AddDataScreen />} />
+          <Route path="/manage" element={<ManagementScreen />} />
+          <Route path="/addData/:dataNumber" element={<AddDataScreen />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
