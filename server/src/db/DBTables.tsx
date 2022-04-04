b283aff77ac9605bd834ec2c970f440659e66be3
LittleWaterDrops <nic6@dangamsoft.com>
2022-04-04 16:23:32 +0900

    server down

--- server/src/db/DBTables.tsx
+++ server/src/db/DBTables.tsx
@@ -3,7 +3,7 @@ const CURRENT_MONTH = new Date().getMonth() + 1
 const DBTables = {
   MEMBER_LIST: "DGmembers",
   USAGE_LIST: "card_usage_statement",
-  USE_DATA: `DGcard_use_data_220${CURRENT_MONTH}`,
+  USE_DATA: `DGcard_use_data_2204`,
 }
 
 export default DBTables
