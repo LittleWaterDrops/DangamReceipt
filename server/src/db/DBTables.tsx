c81c0e86bd3557e2f54b6aa129a378e0ecd771b3
LittleWaterDrops <nic6@dangamsoft.com>
2022-10-04 13:14:06 +0900

    데이터베이스 테이블 이름 정의 시 달에 자동으로 0 붙이기

--- server/src/db/DBTables.tsx
+++ server/src/db/DBTables.tsx
@@ -3,7 +3,7 @@ const CURRENT_MONTH = new Date().getMonth() + 1
 const DBTables = {
   MEMBER_LIST: "DGmembers",
   USAGE_LIST: "card_usage_statement",
-  USE_DATA: `DGcard_use_data_220${CURRENT_MONTH}`,
+  USE_DATA: `DGcard_use_data_22${("00" + CURRENT_MONTH.toString()).slice(-2)}`,
 }
 
 export default DBTables
